a0a720cd9e4a2e471b969527bd453082d00ed00d
Dmitry Kiselev <kiselevdv@mediatech.dev>
2021-01-07 01:29:16 +0300

    chapter 4: page 113

--- account/views.py
+++ account/views.py
@@ -1,3 +1,56 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import LoginForm, UserRegistrationForm
+
+
+def user_login(request):
+    """Вход на сайт"""
+    if request.method == 'POST':
+        # Создаём объект формы с данными
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # Сравниваем с даннми в БД
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    # Авторизуем пользователя на сайте и сохраняем его в сессии
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login or password')
+    else:
+        form = LoginForm()
+
+    return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+def register(request):
+    """Регистрация пользователя"""
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Создаём нового пользователя, но не сохраняем его в БД
+            new_user = user_form.save(commit=False)
+            # Задаём пользователю зашифрованный пароль т.е. сохраняем не напрямую код в БД
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Сохраняем пользователя в БД
+            new_user.save()
+            return render(request, 'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(request, 'account/register.html', {'user_form': user_form})
